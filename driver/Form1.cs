1ebbba7f895aec5059ec66f3666bf4a84fe4a97b
lights li <lightsever@hotmail.com>
2017-07-11 21:29:02 +0800

    a

---
+++ driver/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace driver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x4e:
+                case 0xd:
+                case 0xe:
+                case 0x14:
+                    base.WndProc(ref m);
+                    break;
+                case 0x84://鼠标点任意位置后可以拖动窗体
+                    this.DefWndProc(ref m);
+                    if (m.Result.ToInt32() == 0x01)
+                    {
+                        m.Result = new IntPtr(0x02);
+                    }
+                    break;
+                case 0xA3://禁止双击最大化
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+
+    }
+}
