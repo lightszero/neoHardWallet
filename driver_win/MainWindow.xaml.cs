51cf219ce8832004aeecf28d0a3b174e2298c158
lights <lightsever@hotmail.com>
2017-07-28 23:25:01 +0800

    a

---
+++ driver_win/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace driver_win
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer(TimeSpan.FromSeconds(1.0), System.Windows.Threading.DispatcherPriority.Normal, (s, e1) =>
+              {
+                  this.CheckDevice();
+              }, this.Dispatcher);
+
+            CheckDevice();
+            USBHIDDriver.StartRead((bs) =>
+            {
+                Action call = () =>
+                 {
+                     if(bs[1]==0x31)
+                     {
+                         this.list1.Items.Add("同意交易");
+                     }
+                     else
+                     {
+                         this.list1.Items.Add("拒绝交易");
+
+                     }
+
+                 };
+                this.Dispatcher.Invoke(call);
+            });
+        }
+        void CheckDevice()
+        {
+            var signerCount= USBHIDDriver.CheckDevice();
+            this.info1.Content = signerCount.ToString();
+            if (USBHIDDriver.IsActive())
+                USBHIDDriver.Send("abc");
+        }
+    }
+
+
+}
